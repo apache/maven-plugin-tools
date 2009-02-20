23b1cd791fb54f21284f530c5533f84ffc818615
Benjamin Bentmann <bentmann@apache.org>
2009-02-20 22:26:33 +0000

    [MPLUGIN-100] Allow customization of file encoding used for generated help goal
    o Used encoding from request for output file
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugin-tools/trunk@746389 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugin-tools-api/src/main/java/org/apache/maven/tools/plugin/generator/PluginHelpGenerator.java
+++ maven-plugin-tools-api/src/main/java/org/apache/maven/tools/plugin/generator/PluginHelpGenerator.java
@@ -20,9 +20,10 @@ package org.apache.maven.tools.plugin.generator;
  */
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Date;
@@ -123,7 +124,7 @@ public class PluginHelpGenerator
         Writer writer = null;
         try
         {
-            writer = new FileWriter( helpClass );
+            writer = new OutputStreamWriter( new FileOutputStream( helpClass ), request.getEncoding() );
             writeClass( writer, pluginDescriptor, helpDescriptor );
             writer.flush();
         }
