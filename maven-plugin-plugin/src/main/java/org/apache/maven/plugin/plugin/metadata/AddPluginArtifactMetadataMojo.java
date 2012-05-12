45f4ad60342f523878f0fdeb0ba4165ff6146e0c
Herve Boutemy <hboutemy@apache.org>
2012-05-12 10:01:00 +0000

    improved javadoc
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugin-tools/branches/MPLUGIN-189@1337493 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugin-plugin/src/main/java/org/apache/maven/plugin/plugin/metadata/AddPluginArtifactMetadataMojo.java
+++ maven-plugin-plugin/src/main/java/org/apache/maven/plugin/plugin/metadata/AddPluginArtifactMetadataMojo.java
@@ -29,10 +29,17 @@ import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.project.MavenProject;
 
 /**
- * Inject any plugin-specific artifact metadata to the project's artifact, for subsequent installation
- * and deployment. The first use-case for this is to add the LATEST metadata (which is plugin-specific)
- * for shipping alongside the plugin's artifact.
+ * Inject any plugin-specific
+ * <a href="/ref/current/maven-repository-metadata/repository-metadata.html">artifact metadata</a> to the project's
+ * artifact, for subsequent installation and deployment.
+ * It is used:
+ * <ol>
+ * <li>to add the <code>latest</code> metadata (which is plugin-specific) for shipping alongside the plugin's artifact</li>
+ * <li>to define plugin mapping in the group</li>
+ * </ol>
  *
+ * @see ArtifactRepositoryMetadata
+ * @see GroupRepositoryMetadata
  * @version $Id$
  * @since 2.0
  * @phase package
@@ -43,7 +50,7 @@ public class AddPluginArtifactMetadataMojo
     extends AbstractMojo
 {
     /**
-     * The project artifact, which should have the LATEST metadata added to it.
+     * The project artifact, which should have the <code>latest</code> metadata added to it.
      *
      * @parameter default-value="${project}"
      * @required
