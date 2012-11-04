1de1779688deadf30f73ed5a33471fc0337de828
Robert Scholte <rfscholte@apache.org>
2012-11-04 19:37:07 +0000

    Add definitionList of special Maven objects with their class
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugin-tools/trunk@1405624 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugin-annotations/src/main/java/org/apache/maven/plugins/annotations/Component.java
+++ maven-plugin-annotations/src/main/java/org/apache/maven/plugins/annotations/Component.java
@@ -30,8 +30,14 @@ import java.lang.annotation.Target;
  * Used to configure injection of Plexus components by
  * <a href="/ref/current/maven-core/apidocs/org/apache/maven/plugin/MavenPluginManager.html">
  * <code>MavenPluginManager.getConfiguredMojo(...)</code></a> and special Maven
- * objects as well: <code>session</code>, <code>project</code>, <code>mojoExecution</code>, <code>plugin</code> (Maven-3 only),
- * <code>settings</code>.
+ * objects as well:
+ * <dl>
+ *   <dt>mojoExecution</dt>        <dd>org.apache.maven.plugin.MojoExecution</dd>
+ *   <dt>project</dt>              <dd>org.apache.maven.project.MavenProject</dd>
+ *   <dt>session</dt>              <dd>org.apache.maven.execution.MavenSession</dd>
+ *   <dt>settings</dt>             <dd>org.apache.maven.settings.Settings</dd>
+ *   <dt>plugin (Maven-3 only)</dt><dd>org.apache.maven.plugin.descriptor.PluginDescriptor</dd>
+ * </dl>
  *
  * @author Olivier Lamy
  * @since 3.0
