92f5eaaa3d010783e6c9447fda5d87ae9a32cf28
Christian Schulte <schulte@apache.org>
2017-02-25 04:14:32 +0000

    o Updated to stop suppressing exceptions when closing resources fails.
    git-svn-id: https://svn.apache.org/repos/asf/maven/plugin-tools/trunk@1784346 13f79535-47bb-0310-9956-ffa450edef68

--- maven-script/maven-plugin-tools-model/src/main/java/org/apache/maven/tools/plugin/extractor/model/PluginMetadataParser.java
+++ maven-script/maven-plugin-tools-model/src/main/java/org/apache/maven/tools/plugin/extractor/model/PluginMetadataParser.java
@@ -78,6 +78,9 @@ public class PluginMetadataParser
                     descriptors.add( descriptor );
                 }
             }
+
+            reader.close();
+            reader = null;
         }
         catch ( IOException e )
         {
