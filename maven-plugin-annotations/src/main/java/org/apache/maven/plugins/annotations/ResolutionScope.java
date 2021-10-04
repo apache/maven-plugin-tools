71f2528ebd40bc4f2959272a7f103db75844d9ce
Tamas Cservenak <tamas@cservenak.net>
2021-10-04 17:22:01 +0200

    Just inline strings, no need for constants

--- maven-plugin-annotations/src/main/java/org/apache/maven/plugins/annotations/ResolutionScope.java
+++ maven-plugin-annotations/src/main/java/org/apache/maven/plugins/annotations/ResolutionScope.java
@@ -23,6 +23,9 @@ package org.apache.maven.plugins.annotations;
  * Dependencies resolution scopes available before
  * <a href="/ref/current/maven-core/apidocs/org/apache/maven/lifecycle/internal/MojoExecutor.html">mojo execution</a>.
  *
+ * Important note: The {@code id} values of this enum corresponds to constants of
+ * {@code org.apache.maven.artifact.Artifact} class and MUST BE KEPT IN SYNC (not that they changed recently).
+ *
  * @author Hervé Boutemy
  * @since 3.0
  */
@@ -36,40 +39,28 @@ public enum ResolutionScope
      * <code>compile</code> resolution scope
      * = <code>compile</code> + <code>system</code> + <code>provided</code> dependencies
      */
-    COMPILE( ResolutionScope.SCOPE_COMPILE ),
+    COMPILE( "compile" ),
     /**
      * <code>compile+runtime</code> resolution scope (Maven 3 only)
      * = <code>compile</code> + <code>system</code> + <code>provided</code> + <code>runtime</code> dependencies
      */
-    COMPILE_PLUS_RUNTIME( ResolutionScope.SCOPE_COMPILE_PLUS_RUNTIME ),
+    COMPILE_PLUS_RUNTIME( "compile+runtime" ),
     /**
      * <code>runtime</code> resolution scope
      * = <code>compile</code> + <code>runtime</code> dependencies
      */
-    RUNTIME( ResolutionScope.SCOPE_RUNTIME ),
+    RUNTIME( "runtime" ),
     /**
      * <code>runtime+system</code> resolution scope (Maven 3 only)
      * = <code>compile</code> + <code>system</code> + <code>runtime</code> dependencies
      */
-    RUNTIME_PLUS_SYSTEM( ResolutionScope.SCOPE_RUNTIME_PLUS_SYSTEM ),
+    RUNTIME_PLUS_SYSTEM( "runtime+system" ),
     /**
      * <code>test</code> resolution scope
      * = <code>compile</code> + <code>system</code> + <code>provided</code> + <code>runtime</code> + <code>test</code>
      * dependencies
      */
-    TEST( ResolutionScope.SCOPE_TEST );
-
-    // -- this block below MUST BE KEPT IN SYNC with org.apache.maven.artifact.Artifact constants!
-    private static final String SCOPE_COMPILE = "compile";
-
-    private static final String SCOPE_COMPILE_PLUS_RUNTIME = "compile+runtime";
-
-    private static final String SCOPE_TEST = "test";
-
-    private static final String SCOPE_RUNTIME = "runtime";
-
-    private static final String SCOPE_RUNTIME_PLUS_SYSTEM = "runtime+system";
-    // -- this block above MUST BE KEPT IN SYNC with org.apache.maven.artifact.Artifact constants!
+    TEST( "test" );
 
     private final String id;
 
