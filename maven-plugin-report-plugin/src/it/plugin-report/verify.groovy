1c5e07182ac0754a54dbdb6b14d6fb0fcc9d3d38
Tamas Cservenak <tamas@cservenak.net>
2024-04-29 15:22:40 +0200

    Fix IT

--- maven-plugin-report-plugin/src/it/plugin-report/verify.groovy
+++ maven-plugin-report-plugin/src/it/plugin-report/verify.groovy
@@ -26,7 +26,7 @@ assert !pluginInfo.text.contains('Memory')
 assert !pluginInfo.text.contains('Disk Space')
 // check JDK and Maven requirements
 assert pluginInfo.text.contains('1.8')
-assert pluginInfo.text.contains('3.6.3')
+assert pluginInfo.text.contains('3.9.6')
 
 // deprecated info and description
 assert pluginInfo.text.contains('<div><strong>Deprecated.</strong> You don\'t use test goals, do you?</div><br />')
